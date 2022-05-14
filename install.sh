6ef7eb478e4087fb32698361f737a4c4dde23345
Thomas Venturini <tjventurini@users.noreply.github.com>
2022-05-14 22:39:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ HELPER_FILE_PATH="/usr/local/bin/$BACKUP_HELPER_NAME"
 wget -O $HELPER_FILE_PATH https://raw.githubusercontent.com/tjventurini/single-file-backup-helper/master/helper.sh
 
 # Make sure the helper is executable
-chmod u+x $HELPER_FILE_PATH
+chmod +x $HELPER_FILE_PATH
